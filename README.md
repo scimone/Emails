52d77f2010ab507fbc0e133b08f92c649219b7b8
Dalcio <dalciosan@gmail.com>
2023-07-12 23:21:37 +0200

    feat: run as a webservice

--- README.md
+++ README.md
@@ -7,19 +7,18 @@ A simple script with the following functions:
 
 # Requirements 
 pip3 install -U scikit-learn
-
+pip3 install flask
 pip3 install bs4
+pip3 install flask
 
 
 # Run
-To use the code, follow these steps:
 
 ## Optional: Train the classifier
 1. Put a data folder inside the root of the project with two subfolders "1. automated" and "2. human"
-1. Run the train.py script to train and save the model as "email_classification_model.joblib":
+2. Run the train.py script to train and save the model as "email_classification_model.joblib":
    `python3 train.py`
 
 ## Classify an email
-1. Place the email file that you want to classify inside the root and update the email_file_path variable in the second script with the path to that file.
-2. Run the predict.py script to classify the email:
-   `python3 predict.py`
+1. Run `flask run` in the main root, that will start a service in `http://127.0.0.1:5000/`
+2. Use `[POST] /predict` endpoint to predict new email
---
+++ app.py
@@ -0,0 +1,50 @@
+import re
+import joblib
+from bs4 import BeautifulSoup
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Load the trained model
+pipeline = joblib.load('email_classification_model.joblib')
+
+def extract_text_from_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    text = soup.get_text(separator=' ')
+    # Remove extra whitespaces and newlines
+    clean_text = re.sub('\s+', ' ', text).strip()
+    return clean_text
+
+@app.route('/predict', methods=['POST'])
+def predict_email():
+    try:
+        # Check if the request has the correct API key
+        api_key = request.headers.get('x-api-key')
+        if api_key != 'lvlScmd2Oup2cmjYQkw12gASiYwLBZzd':
+            return jsonify({'error': 'Invalid API key'}), 401
+
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file uploaded'}), 400
+
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({'error': 'No file selected'}), 400
+
+        # Read the file and extract text from HTML
+        html = file.read().decode('utf-8')
+        text = extract_text_from_html(html)
+
+        # Classify the email
+        predicted_label = pipeline.predict([text])[0]
+
+        # Return the prediction as JSON
+        return jsonify({'result': True, 'data': {
+            'predicted_label': predicted_label,
+            'text': text
+        }})
+    except Exception as e:
+        return jsonify({'result': False, 'error': str(e)}), 500
+    
+
+if __name__ == '__main__':
+    app.run()
